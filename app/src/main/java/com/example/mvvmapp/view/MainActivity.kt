69246774c9e62bedcc1d54145ddbcebee79793ab
ppraveen92 <ppraveen92@gmail.com>
2020-11-26 17:09:10 +0530

    Before Review

--- app/src/main/java/com/example/mvvmapp/view/MainActivity.kt
+++ app/src/main/java/com/example/mvvmapp/view/MainActivity.kt
@@ -13,33 +13,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-      /*  var number : Int =0
-
-        val viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java);
-
-        txt.text=viewModel.number.toString()
-        btn.setOnClickListener {
-           *//* number++
-            txt.text=number.toString()*//*
-
-            viewModel.increaseNumber()
-            txt.text=viewModel.number.toString()
-
-        }*/
-
-
-        val viewModelLive =ViewModelProviders.of(this).get(LiveDataViewModel::class.java)
+        val viewModelLive = ViewModelProviders.of(this).get(LiveDataViewModel::class.java)
         viewModelLive.setLiveCount.value = 20000
         viewModelLive.setTimer()
         viewModelLive.getLiveData().observe(this, Observer {
-            txtLive.text=it.toString()
+            txtLive.text = it.toString()
         })
-
         viewModelLive.getFinish().observe(this, Observer {
-
-            Toast.makeText(this,it,Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
         })
-
-
     }
 }
--- app/src/main/java/com/example/mvvmapp/viewmodel/LiveDataViewModel.kt
+++ app/src/main/java/com/example/mvvmapp/viewmodel/LiveDataViewModel.kt
@@ -5,37 +5,29 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class LiveDataViewModel : ViewModel()
-{
-    private lateinit var timer:CountDownTimer
+class LiveDataViewModel : ViewModel() {
+    private lateinit var timer: CountDownTimer
     var countLiveData = MutableLiveData<Int>()
-     var finishLivedata = MutableLiveData<String>()
+    var finishLivedata = MutableLiveData<String>()
     var setLiveCount = MutableLiveData<Long>()
-
-    fun getLiveData():MutableLiveData<Int>
-    {
+    fun getLiveData(): MutableLiveData<Int> {
         return countLiveData
     }
 
-    fun getFinish():LiveData<String>
-    {
+    fun getFinish(): LiveData<String> {
         return finishLivedata
     }
 
-
-    fun setTimer()
-    {
-        timer = object :CountDownTimer(setLiveCount.value!!,1000){
+    fun setTimer() {
+        timer = object : CountDownTimer(setLiveCount.value!!, 1000) {
             override fun onTick(millisUntilFinished: Long) {
-                val time = millisUntilFinished/1000
-                countLiveData.value=time.toInt()
-
+                val time = millisUntilFinished / 1000
+                countLiveData.value = time.toInt()
             }
 
             override fun onFinish() {
                 finishLivedata.value = "done"
             }
-
         }.start()
     }
 }
